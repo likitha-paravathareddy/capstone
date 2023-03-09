606dc7b14478b577968a7e4e292cf5bfc6743ce4
likitha.p <likitha.p@darwinbox.io>
2023-03-09 16:16:30 +0530

    attendance

--- server/mvc/models/attendanceModel.js
+++ server/mvc/models/attendanceModel.js
@@ -9,29 +9,10 @@ const attendanceSchema=mongoose.Schema({
     {
         type:String
     },
-    sub1:{
-        type:String
-    },
-    sub2:
-    {
-        type:String
-    },
-    sub3:
-    {
-        type:String
-    },
-    sub4:
-    {
-        type:String
-    },
-    sub5:
-    {
-        type:String
-    },
-    sub6:
-    {
-        type:String
-    }    
+    sub:{
+        type:[Boolean]
+    }
+
 })
 
 let attendanceModel=mongoose.model('attendance',attendanceSchema)
