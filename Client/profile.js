2f42f50712242d368a6da44295087717db122871
saloni-dayal <123060770+saloni-dayal@users.noreply.github.com>
2023-03-09 16:48:38 +0530

    Update profile.js

--- Client/profile.js
+++ Client/profile.js
@@ -2,3 +2,44 @@ var loadFile = function (event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
   };
+
+  $("#submit-details").click(() =>
+  {
+    contact = $("#contact").val();
+    dob = $("#dob").val();
+    gender = $("#gender").val();
+    address = $("#address").val();
+    gname = $("#gname").val();
+    gcontact = $("#gcontact").val();
+    exam = $("#exam").val();
+    rank = $("#examrank").val();
+    
+  })
+
+  function submitDetails()
+  {
+    var data =
+    {
+      contact: $("#contact").val(),
+    dob: $("#dob").val(),
+    gender: $("#gender").val(),
+    address: $("#address").val(),
+    gname: $("#gname").val(),
+    gcontact: $("#gcontact").val(),
+    exam: $("#exam").val(),
+    rank: $("#examrank").val()
+    }
+    $.ajax({
+      type: "post",
+      url: "http://localhost:8080/profile",
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      xhrFields: {
+        withCredentials: false,
+      },
+      
+    });
+  }
+
+
+
